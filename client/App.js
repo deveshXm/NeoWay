674bbd1c323a1ed5c4fdafda17d2cd83c461658a
ScriptedPranav <sinhafighterpranav@gmail.com>
2023-11-03 23:25:59 +0530

    feat:Add Stack Navigator

--- client/App.js
+++ client/App.js
@@ -1,15 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import StackNavigator from "./src/navigation/StackNavigator";
 import Context from "./src/context/context";
 
 export default function App() {
   return (
     <Context.Provider>
       <View style={styles.container}>
-        <SafeAreaView>
-          <StatusBar backgroundColor="#1F1F1F" style="light" />
-        </SafeAreaView>
+        <NavigationContainer>
+          <SafeAreaView>
+            <StatusBar backgroundColor="#1F1F1F" style="light" />
+            <StackNavigator />
+          </SafeAreaView>
+        </NavigationContainer>
       </View>
     </Context.Provider>
   );
