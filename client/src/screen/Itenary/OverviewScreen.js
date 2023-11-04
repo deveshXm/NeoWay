9568f86683a90bf43da4008bae907936e041aabe
NIKU-SINGH <soapmactavishmw4@gmail.com>
2023-11-05 03:46:48 +0530

    patch: updated spacing in the overview page

--- client/src/screen/Itenary/OverviewScreen.js
+++ client/src/screen/Itenary/OverviewScreen.js
@@ -2,32 +2,36 @@ import { ScrollView, Dimensions, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import common from "../../../util/common";
 import CarouselComponent from "../../components/cards/carouselComponent";
-import data from "../../../util/data/itenary.json";
+import itenaryData from "../../../util/data/itenary.json";
 
 const OverviewScreen = () => {
   return (
     <>
       <ScrollView>
         <View style={styles.container1}>
-          <Text style={styles.subHeading1}>{data[0].title}</Text>
-          <Text style={styles.content}>{data[0].description}</Text>
-          {/* Day1 */}
+          <View style={styles.alignmentContainer}>
+            <Text style={styles.subHeading1}>{itenaryData[0].title}</Text>
+            <Text style={styles.content}>{itenaryData[0].description}</Text>
 
-          {data[0].itenerary.map((item, index) => (
-            <View key={index} style={styles.container2}>
-              <Text style={styles.subHeading2}>{item.day}</Text>
-              <Text style={styles.content}>{item.description}</Text>
-              {item.activities.map((activity, index) => (
-                <View key={index}>
-                  <View style={styles.container1}>
-                    <Text style={styles.subHeading3}>{activity.name}</Text>
-                    <Text style={styles.content}>{activity.description} </Text>
+            {itenaryData[0].itenary.map((item, index) => (
+              <View key={index} style={styles.container2}>
+                <Text style={styles.subHeading2}>{item.day}</Text>
+                <Text style={styles.content}>{item.description}</Text>
+                {item.activities.map((activity, index) => (
+                  <View key={index}>
+                    <View style={styles.container1}>
+                      <Text style={styles.subHeading3}>{activity.name}</Text>
+                      <Text style={styles.content}>
+                        {activity.description}{" "}
+                      </Text>
+                    </View>
+                    {/* Send the data activity.images (Which is an arary of images) */}
+                    <CarouselComponent />
                   </View>
-                  <CarouselComponent />
-                </View>
-              ))}
-            </View>
-          ))}
+                ))}
+              </View>
+            ))}
+          </View>
         </View>
       </ScrollView>
     </>
@@ -52,27 +56,27 @@ const styles = StyleSheet.create({
     backgroundColor: common.color.backgroundPrimary,
     marginHorizontal: 0,
   },
+  alignmentContainer: {
+    marginHorizontal: common.sizes.ml,
+  },
   subHeading1: {
     fontSize: common.sizes.ml,
-    marginLeft: common.sizes.l,
     color: common.color.subHeading,
     fontFamily: common.text.poppinsSemiBold,
   },
   subHeading2: {
     fontSize: common.sizes.m,
-    marginLeft: common.sizes.l,
+    // marginLeft: common.sizes.l,
     color: common.color.subHeading,
     fontFamily: common.text.poppinsSemiBold,
   },
   subHeading3: {
     fontSize: common.sizes.ms,
-    marginLeft: common.sizes.l,
     color: common.color.subHeading2,
     fontFamily: common.text.poppinsSemiBold,
   },
   content: {
     fontSize: common.sizes.mxs,
-    marginLeft: common.sizes.l,
     color: common.color.content,
     fontFamily: common.text.poppinsRegular,
   },
