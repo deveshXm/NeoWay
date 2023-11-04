bcd4bb8f6cae7c70ce21e2c287949e9f6898d3ef
Devesh <nothefakedevesh@gmail.com>
2023-11-05 05:18:21 +0530

    feat: added bot button in iternatry

--- client/src/screen/Itenary/ItenaryScreen.js
+++ client/src/screen/Itenary/ItenaryScreen.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
 
 import common from "../../../util/common";
 import Text from "../../components/common/Text";
 import ItenaryNavigation from "../../navigation/ItenaryNavigation";
 import { useNavigation } from "@react-navigation/native";
 import ItenaryLoadingScreen from "../loading/ItenaryLoadingScreen";
+import { TouchableHighlight } from "react-native-gesture-handler";
 
 const ItenaryScreen = () => {
   const [loading, setLoading] = useState(true);
   const navigation = useNavigation();
+
+  const handleChat = () => {
+    navigation.navigate("Chat");
+  };
 
   useEffect(() => {
     (async () => {
       await new Promise((resolve, reject) => {
         setTimeout(() => {
           setLoading(false);
-          resolve();  
+          resolve();
         }, 2000);
       });
     })();
@@ -28,6 +33,16 @@ const ItenaryScreen = () => {
     <View style={styles.container}>
       <Text style={styles.subHeading}>Selected Itenaries</Text>
       <Text style={styles.subHeading1}>Total Results: 20+</Text>
+      <View style={styles.bot}>
+        <TouchableHighlight
+          underlayColor="transparent" // No color is shown under the component when pressed
+          activeOpacity={1}
+          onPress={handleChat}
+          style={styles.button}
+        >
+          <Image source={require("../../../assets/bot.png")} />
+        </TouchableHighlight>
+      </View>
       <ItenaryNavigation />
     </View>
   );
@@ -38,7 +53,7 @@ export default ItenaryScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop:20,
+    paddingTop: 20,
     width: Dimensions.width,
     backgroundColor: common.color.backgroundPrimary,
     marginHorizontal: 0,
@@ -47,14 +62,26 @@ const styles = StyleSheet.create({
     marginLeft: common.sizes.l,
     textAlign: "flex-start",
     fontSize: common.sizes.ml,
-    color:common.color.subHeading,
+    color: common.color.subHeading,
     fontFamily: common.text.poppinsSemiBold,
   },
   subHeading1: {
     marginLeft: common.sizes.l,
     fontSize: common.sizes.ms,
-    color:common.color.subHeading,
+    color: common.color.subHeading,
     fontFamily: common.text.poppinsMedium,
-    marginTop:-5,
+    marginTop: -5,
+  },
+  bot: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+  },
+  button: {
+    borderRadius: 100,
+    borderColor: "black",
+    borderWidth: StyleSheet.hairlineWidth,
+    width: 60,
+    height: 60,
   },
 });
