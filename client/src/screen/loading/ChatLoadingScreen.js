ebebb215f34efecd15b34a2f84f04fd200a447c0
Devesh <nothefakedevesh@gmail.com>
2023-11-04 15:04:17 +0530

    refactor: named change and chat

---
+++ client/src/screen/loading/ChatLoadingScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ChatLoadingScreen = () => {
+  return (
+    <View>
+      <Text>ChatLoadingScreen</Text>
+    </View>
+  )
+}
+
+export default ChatLoadingScreen
+
+const styles = StyleSheet.create({})
