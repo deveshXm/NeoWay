4af1f40ded9140963da94f693cdd94339b3b0359
Devesh <nothefakedevesh@gmail.com>
2023-11-04 16:00:01 +0530

    refactor: itenary loading screen initialized

--- client/src/screen/ChatScreen.js
+++ client/src/screen/ChatScreen.js
@@ -3,9 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 
 import common from "../../util/common";
 import ChatLoadingScreen from "./loading/ChatLoadingScreen";
+import Button from "../components/common/Button";
+import { useNavigation } from "@react-navigation/native";
 
 const ChatScreen = () => {
   const [loading, setLoading] = useState(true);
+  const navigation = useNavigation()
+
+  const handleOnClick = () => {
+    navigation.navigate("Itenary")
+  }
 
   useEffect(() => {
     (async () => {
@@ -23,6 +30,7 @@ const ChatScreen = () => {
   ) : (
     <View style={styles.container}>
       <Text>ChatScreen</Text>
+      <Button title="sdfs" onPress={handleOnClick}/>
     </View>
   );
 };
---
+++ client/src/screen/loading/ItenaryLoadingScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ItenaryLoadingScreen = () => {
+  return (
+    <View>
+      <Text>ItenaryLoadingScreen</Text>
+    </View>
+  )
+}
+
+export default ItenaryLoadingScreen
+
+const styles = StyleSheet.create({})
